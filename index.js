bde301a2ad2bc9344da5be6463e1f808b8f4c58a
Davis Barber <barberdt@gmail.com>
2015-09-07 21:58:46 -0400

    Prepare for npm publish

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
 'use strict';
 
+const FieldValidator = require('./lib/FieldValidator');
 
-module.exports = require('./lib');
+
+/**
+ * Generator-returning function to be used with app.use. Provides the validate
+ * method to the app instance.
+ *
+ * @return {Generator} The app middleware.
+ */
+module.exports = function() {
+
+  /**
+   * Generator function middleware.
+   *
+   * @param {Generator} next - The next middleware in the chain to yield to.
+   */
+  return function *(next) {
+    const ctx = this;
+
+    /**
+     * Take the given field name and create and return a FieldValidator for it.
+     *
+     * @param {String} fieldName - The name of the field on the request body.
+     * @return {FieldValidator} The field validator class for the field.
+     */
+    this.validate = function(fieldName) {
+      // This middleware requires that koa-body has parsed the request body.
+      if (!ctx.request.body) {
+        ctx.throw('Validate requires a parsed body.');
+      }
+
+      return new FieldValidator(fieldName, ctx);
+    };
+
+    yield next;
+  }
+};
